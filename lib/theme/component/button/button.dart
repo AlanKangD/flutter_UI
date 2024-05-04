48e1b3cd19805c63404b9fcd6ae567533c3fefea
alan <kno12350@gmail.com>
2024-05-04 17:46:12 +0900

    외부로부터 전달 받는 파라이터 변수 선언 완료 _ button 커스텀 아이징

--- lib/theme/component/button/button.dart
+++ lib/theme/component/button/button.dart
@@ -3,3 +3,34 @@ import 'package:house_of_tomorrow/src/service/theme_service.dart';
 
 part 'button_size.dart';
 part 'button_type.dart';
+
+class Button extends StatelessWidget {
+  const Button({super.key});
+
+  //클릭 이벤트
+  final void Function() onPressed;
+
+  // Button 타입 & 크기
+  final ButtonType type;
+  final ButtonSize size;
+
+  // 비활성화 여부
+  final bool isInactive;
+
+  // 텍스트 & 아이콘
+  final String? text;
+  final String? icon;
+
+  // 폭
+  final double? width;
+
+  // 커스텀 색상
+  final Color? color;
+  final Color? backgroundColor;
+  final Color? borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
