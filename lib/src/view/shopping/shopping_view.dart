3fe9ae1ed2d99b21f9393a2fc549e843f4e9b5c5
alan <kno12350@gmail.com>
2024-05-08 13:58:01 +0900

    구현한 키보드를 사라지게 하는 기능 위젯트리에 등록

--- lib/src/view/shopping/shopping_view.dart
+++ lib/src/view/shopping/shopping_view.dart
@@ -7,6 +7,7 @@ import 'package:house_of_tomorrow/src/view/shopping/widget/product_card_grid.dar
 import 'package:house_of_tomorrow/src/view/shopping/widget/product_empty.dart';
 import 'package:house_of_tomorrow/theme/component/bottom_sheet/setting_bottom_sheet.dart';
 import 'package:house_of_tomorrow/theme/component/button/button.dart';
+import 'package:house_of_tomorrow/theme/component/hide_keyboard.dart';
 import 'package:house_of_tomorrow/theme/component/input_field.dart';
 import 'package:house_of_tomorrow/util/helper/network_helper.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
@@ -57,63 +58,65 @@ class _ShoppingViewState extends State<ShoppingView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(S.current.shopping),
-          actions: [
-            // 설정 버튼
-            Button(
-              icon: 'option',
-              type: ButtonType.flat,
-              onPressed: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return const SettingBottomSheet();
-                  },
-                );
-              },
-            )
-          ],
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              child: Row(
-                children: [
-                  // 검색
-                  Expanded(
-                    child: InputField(
-                      controller: textcontroller,
-                      onClear: searchProductList,
-                      onSubmitted: (text) => searchProductList(),
-                      hint: S.current.searchProduct,
+    return HideKeyboard(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(S.current.shopping),
+            actions: [
+              // 설정 버튼
+              Button(
+                icon: 'option',
+                type: ButtonType.flat,
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return const SettingBottomSheet();
+                    },
+                  );
+                },
+              )
+            ],
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: Row(
+                  children: [
+                    // 검색
+                    Expanded(
+                      child: InputField(
+                        controller: textcontroller,
+                        onClear: searchProductList,
+                        onSubmitted: (text) => searchProductList(),
+                        hint: S.current.searchProduct,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-
-                  //검색 버튼
-                  Button(
-                    icon: 'search',
-                    onPressed: searchProductList,
-                  )
-                ],
+                    const SizedBox(
+                      width: 16,
+                    ),
+      
+                    //검색 버튼
+                    Button(
+                      icon: 'search',
+                      onPressed: searchProductList,
+                    )
+                  ],
+                ),
               ),
-            ),
-
-            // ProductCardList
-            Expanded(
-              child: productList.isEmpty
-                  ? const ProductEmpty()
-                  : ProductCardGrid(productList),
-            ),
-          ],
-        ));
+      
+              // ProductCardList
+              Expanded(
+                child: productList.isEmpty
+                    ? const ProductEmpty()
+                    : ProductCardGrid(productList),
+              ),
+            ],
+          )),
+    );
   }
 }
