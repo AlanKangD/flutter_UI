ad68acb8520d24cbece0ad910df7a574401bbc83
alan <kno12350@gmail.com>
2024-05-08 14:10:20 +0900

    상세페이지로 변환하기 위한 product_card 에서 router 설정 완료

--- lib/src/view/shopping/widget/product_card.dart
+++ lib/src/view/shopping/widget/product_card.dart
@@ -4,6 +4,7 @@ import 'package:house_of_tomorrow/src/model/product.dart';
 import 'package:house_of_tomorrow/src/service/theme_service.dart';
 import 'package:house_of_tomorrow/theme/component/rating.dart';
 import 'package:house_of_tomorrow/util/helper/intl_helper.dart';
+import 'package:house_of_tomorrow/util/route_path.dart';
 
 class ProductCard extends StatelessWidget {
   const ProductCard({
@@ -15,55 +16,64 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: context.color.surface,
-        boxShadow: context.deco.shadow,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (product.productColorList.isNotEmpty)
-            ClipRRect(
-              borderRadius: BorderRadiusDirectional.circular(16),
-              child: Image.network(
-                product.productColorList.first.iamgeUrl,
-                fit: BoxFit.cover,
-              ),
-            ),
-          const SizedBox(height: 4),
-          Text(
-            product.name.toString(),
-            style: context.typo.headline4.copyWith(
-              fontWeight: context.typo.semiBold,
-            ),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            product.brand.toString(),
-            style: context.typo.body2.copyWith(
-              fontWeight: context.typo.light,
-              color: context.color.subtext,
-            ),
-          ),
-          const SizedBox(height: 4),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  IntlHelper.currency(
-                    symbol: product.priceUnit,
-                    number: product.price,
-                  ),
-                  style: context.typo.subtitle2,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          RoutePath.product,
+          arguments: product,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: context.color.surface,
+          boxShadow: context.deco.shadow,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (product.productColorList.isNotEmpty)
+              ClipRRect(
+                borderRadius: BorderRadiusDirectional.circular(16),
+                child: Image.network(
+                  product.productColorList.first.iamgeUrl,
+                  fit: BoxFit.cover,
                 ),
               ),
-              Rating(rating: product.rating),
-            ],
-          ),
-        ],
+            const SizedBox(height: 4),
+            Text(
+              product.name.toString(),
+              style: context.typo.headline4.copyWith(
+                fontWeight: context.typo.semiBold,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              product.brand.toString(),
+              style: context.typo.body2.copyWith(
+                fontWeight: context.typo.light,
+                color: context.color.subtext,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    IntlHelper.currency(
+                      symbol: product.priceUnit,
+                      number: product.price,
+                    ),
+                    style: context.typo.subtitle2,
+                  ),
+                ),
+                Rating(rating: product.rating),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
