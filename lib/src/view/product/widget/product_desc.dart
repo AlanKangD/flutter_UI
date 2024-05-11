0531c36be1f479b5a44c5556b19e510915d9c5b8
alan <kno12350@gmail.com>
2024-05-11 15:23:30 +0900

    디스크립션 구현 완료

--- lib/src/view/product/widget/product_desc.dart
+++ lib/src/view/product/widget/product_desc.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/src/service/theme_service.dart';
+import 'package:house_of_tomorrow/theme/component/rating.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 
 class ProductDesc extends StatelessWidget {
   const ProductDesc({
@@ -11,6 +15,41 @@ class ProductDesc extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          /// Title
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  S.current.description,
+                  style: context.typo.headline4.copyWith(
+                    fontWeight: context.typo.semiBold,
+                  ),
+                ),
+              ),
+
+              /// Rating
+              Rating(
+                rating: product.rating,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+
+          /// Content
+          Text(
+            product.desc.toString(),
+            style: context.typo.headline6.copyWith(
+              color: context.color.subtext,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
