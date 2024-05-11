49ca2e005cd34fcaf0354ed7f70218ce44ebbd81
alan <kno12350@gmail.com>
2024-05-11 15:16:03 +0900

    상세페이지 디스크립션 기본 폼 구현

---
+++ lib/src/view/product/widget/product_desc.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/model/product.dart';
+
+class ProductDesc extends StatelessWidget {
+  const ProductDesc({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
