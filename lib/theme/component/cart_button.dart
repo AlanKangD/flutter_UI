ccb36a7d780544b1288d68c5cc029a2f8a4685d9
alan <kno12350@gmail.com>
2024-05-11 18:06:23 +0900

    상단의 공통으로 사용할 cart Button 클래스 구현

---
+++ lib/theme/component/cart_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/theme/component/button/button.dart';
+import 'package:house_of_tomorrow/util/route_path.dart';
+
+class CartButton extends StatelessWidget {
+  const CartButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Button(
+      icon: 'basket',
+      type: ButtonType.flat,
+      onPressed: () {
+        Navigator.pushNamed(context, RoutePath.cart);
+      },
+    );
+  }
+}
