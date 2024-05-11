b21469d36fde6cf595a745e6d2f1c9529bcf2fb7
alan <kno12350@gmail.com>
2024-05-11 18:24:14 +0900

    cart Badge 의 값을 cart Service 에서 가지고 온 후에 값 전달 로직 추가

--- lib/theme/component/cart_button.dart
+++ lib/theme/component/cart_button.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/service/cart_service.dart';
 import 'package:house_of_tomorrow/theme/component/button/button.dart';
 import 'package:house_of_tomorrow/theme/component/counter_badge.dart';
 import 'package:house_of_tomorrow/util/route_path.dart';
+import 'package:provider/provider.dart';
 
 class CartButton extends StatelessWidget {
   const CartButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    int count = context.watch<CartService>().cartItemList.length;
     return CounterBadge(
-      isShow: false,
-      label: '0',
+      isShow: count > 0,
+      label: '$count',
       child: Button(
         icon: 'basket',
         type: ButtonType.flat,
