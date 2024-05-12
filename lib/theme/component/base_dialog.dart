3d6945e460e7b51bba845b91eadab951bf58125a
alan <kno12350@gmail.com>
2024-05-12 14:18:48 +0900

    alertDialog 화면 구성 완료

--- lib/theme/component/base_dialog.dart
+++ lib/theme/component/base_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/service/theme_service.dart';
 
 class BaseDialog extends StatelessWidget {
   const BaseDialog({
@@ -14,6 +15,24 @@ class BaseDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AlertDialog(
+      backgroundColor: context.color.surface,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      titlePadding: const EdgeInsets.all(16),
+      contentPadding: const EdgeInsets.all(16).copyWith(
+        top: title != null ? 0 : 16,
+      ),
+      actionsPadding: const EdgeInsets.all(16),
+      title: title != null
+          ? Text(
+              title!,
+              style: context.typo.headline2.copyWith(
+                fontWeight: context.typo.semiBold,
+              ),
+            )
+          : null,
+      content: content,
+      actions: actions,
+    );
   }
 }
