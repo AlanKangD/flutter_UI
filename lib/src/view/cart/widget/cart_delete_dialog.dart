69d3ae8712a0c2baaa2898e306eb888901de4a49
alan <kno12350@gmail.com>
2024-05-12 14:21:33 +0900

    삭제 기능 위젯 추가

---
+++ lib/src/view/cart/widget/cart_delete_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartDeleteDialog extends StatelessWidget {
+  const CartDeleteDialog({
+    super.key,
+    required this.onDeletePressed,
+  });
+
+  final void Function() onDeletePressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
