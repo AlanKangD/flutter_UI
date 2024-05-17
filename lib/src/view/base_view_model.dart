6dff37139da4184f691180343a2a755c978d699d
alan <kno12350@gmail.com>
2024-05-17 09:38:05 +0900

    현재 비지니스로직이 로딩중이라는 flag 를 심어주는 과정

--- lib/src/view/base_view_model.dart
+++ lib/src/view/base_view_model.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-class BaseViewModel with ChangeNotifier {}
+class BaseViewModel with ChangeNotifier {
+  bool _isBusy = false;
+
+  bool get isBusy => _isBusy;
+
+  set isBusy(bool value) {
+    _isBusy = value;
+    notifyListeners();
+  }
+}
