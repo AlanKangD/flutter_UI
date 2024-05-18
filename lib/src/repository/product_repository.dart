63ad50a3e3da6a5a014db8daa381a16717d91902
alan <kno12350@gmail.com>
2024-05-18 13:00:36 +0900

    데이터 전용 리펙토링 완료

--- lib/src/repository/product_repository.dart
+++ lib/src/repository/product_repository.dart
@@ -1,18 +1,16 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:flutter/material.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
-import 'package:house_of_tomorrow/src/view/base_view_model.dart';
 import 'package:house_of_tomorrow/util/helper/network_helper.dart';
 
 class ProductRepository {
-  Future<> searchProductList() async {
+  Future<List<Product>> searchProductList(String keyword) async {
     try {
       final res = await NetworkHelper.dio.get(
         'https://gist.githubusercontent.com/nero-angela/d16a5078c7959bf5abf6a9e0f8c2851a/raw/04fb4d21ddd1ba06f0349a890f5e5347d94d677e/ikeaSofaDataIBB.json',
       );
-      productList = jsonDecode(res.data).map<Product>((json) {
+      return jsonDecode(res.data).map<Product>((json) {
         return Product.fromJsonm(json);
       }).where((product) {
         /// 키워드가 비어있는 경우 모두 반환
@@ -25,6 +23,7 @@ class ProductRepository {
       }).toList();
     } catch (e, s) {
       log('Failed to searchProductList', error: e, stackTrace: s);
+      return [];
     }
   }
 }
