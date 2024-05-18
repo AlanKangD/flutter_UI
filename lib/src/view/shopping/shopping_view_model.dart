15c6f61a66d3a912bfe789878b70bc5c63e319fc
alan <kno12350@gmail.com>
2024-05-18 13:02:41 +0900

    생성한 데이터 처리 전용 클래스 import

--- lib/src/view/shopping/shopping_view_model.dart
+++ lib/src/view/shopping/shopping_view_model.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/src/repository/product_repository.dart';
 import 'package:house_of_tomorrow/src/view/base_view_model.dart';
 import 'package:house_of_tomorrow/util/helper/network_helper.dart';
 
@@ -10,6 +11,7 @@ class ShoppingViewModel extends BaseViewModel {
   List<Product> productList = [];
 
   TextEditingController textcontroller = TextEditingController();
+  final ProductRepository productRepository = ProductRepository();
 
   String get keyword => textcontroller.text.trim();
 
