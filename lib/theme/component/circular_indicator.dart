e5a1bd60aba46eab6fc1bb216f3d732a48bad79c
alan <kno12350@gmail.com>
2024-05-18 13:27:45 +0900

    로딩 창용 신규 클래스 구현

---
+++ lib/theme/component/circular_indicator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CircularIndicatior extends StatelessWidget {
+  const CircularIndicatior({
+    super.key,
+    required this.child,
+    required this.isBusy,
+  });
+
+  final Widget child;
+  final bool isBusy;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
