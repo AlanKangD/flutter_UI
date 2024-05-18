aafab4aa7c0b8347c6620774bbb3de2158a46205
alan <kno12350@gmail.com>
2024-05-18 14:03:49 +0900

    product 리핑토링 완료

--- lib/src/view/product/product_view_model.dart
+++ lib/src/view/product/product_view_model.dart
@@ -1,3 +1,47 @@
+import 'package:house_of_tomorrow/src/model/cart_item.dart';
+import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/src/service/cart_service.dart';
 import 'package:house_of_tomorrow/src/view/base_view_model.dart';
+import 'package:house_of_tomorrow/theme/component/toast/toast.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 
-class ProductViewModel extends BaseViewModel {}
+class ProductViewModel extends BaseViewModel {
+  ProductViewModel({
+    required this.cartService,
+    required this.product,
+  });
+
+  final CartService cartService;
+
+  final Product product;
+
+  // 선택한 수량
+  int count = 1;
+
+  // 선택한 색상
+  int colorIndex = 0;
+
+  // 수량 업데이트 이벤트 함수
+  void onCountChanged(int newCount) {
+    count = newCount;
+    notifyListeners();
+  }
+
+  // 색상 업데이트 이벤트 함수
+  void onColorIndexChanged(int newColorIndex) {
+    colorIndex = newColorIndex;
+    notifyListeners();
+  }
+
+  // 카트 리스트에 추가 함수
+  void onAddToCartPressed() {
+    final CartItem newCartItem = CartItem(
+      product: product,
+      colorIndex: colorIndex,
+      count: count,
+      isSelected: true,
+    );
+    cartService.add(newCartItem);
+    Toast.show(S.current.productAdded(product.name));
+  }
+}
