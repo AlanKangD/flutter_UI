ee1c97d9eea54d7ce605824a6df0738337f6ec26
alan <kno12350@gmail.com>
2024-05-18 13:03:59 +0900

    기존에 사용하던 데이터 받는 로직 제거 후 생성한 클래스의 메소드를 넣어서 사용하며 keyword 를 매개변수로 보내며 연결 완료

--- lib/src/view/shopping/shopping_view_model.dart
+++ lib/src/view/shopping/shopping_view_model.dart
@@ -16,23 +16,6 @@ class ShoppingViewModel extends BaseViewModel {
   String get keyword => textcontroller.text.trim();
 
   Future<void> searchProductList() async {
-    try {
-      final res = await NetworkHelper.dio.get(
-        'https://gist.githubusercontent.com/nero-angela/d16a5078c7959bf5abf6a9e0f8c2851a/raw/04fb4d21ddd1ba06f0349a890f5e5347d94d677e/ikeaSofaDataIBB.json',
-      );
-      productList = jsonDecode(res.data).map<Product>((json) {
-        return Product.fromJsonm(json);
-      }).where((product) {
-        /// 키워드가 비어있는 경우 모두 반환
-        if (keyword.isEmpty) return true;
-
-        /// name이나 brand에 키워드 포함 여부 확인
-        return "${product.name}${product.brand}"
-            .toLowerCase()
-            .contains(keyword.toLowerCase());
-      }).toList();
-    } catch (e, s) {
-      log('Failed to searchProductList', error: e, stackTrace: s);
-    }
+    await productRepository.searchProductList(keyword);
   }
 }
